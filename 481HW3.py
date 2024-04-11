c6b94c82960b7d6d84c2f38f30ace04565c37a72
zixuan28 <zlin28@uw.edu>
2024-04-11 14:24:51 -0700

    new

---
+++ 481HW3.py
@@ -0,0 +1,7 @@
+#Ex 0:
+def github() -> str:
+    """
+   This code would give you a link to my GitHub Repository page!
+    """
+
+    return "https://github.com/zixuan28/ECON481_HWs/blob/main/481HW3.py"
