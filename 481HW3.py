0751b3cdd1fc453871f0c0185174ef4cf426c672
zixuan28 <zlin28@uw.edu>
2024-04-11 17:12:00 -0700

    hi

--- 481HW3.py
+++ 481HW3.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 #Ex 0:
 def github() -> str:
     """
@@ -5,3 +7,21 @@ def github() -> str:
     """
 
     return "https://github.com/zixuan28/ECON481_HWs/blob/main/481HW3.py"
+
+#Ex 1:
+def import_yearly_data(years: list) -> pd.DataFrame:
+    links = {
+        2022: "https://lukashager.netlify.app/econ-481/data/ghgp_data_2022.xlsx",
+        2021: "https://lukashager.netlify.app/econ-481/data/ghgp_data_2021.xlsx",
+        2020: "https://lukashager.netlify.app/econ-481/data/ghgp_data_2020.xlsx",
+        2019: "https://lukashager.netlify.app/econ-481/data/ghgp_data_2019.xlsx"
+    }
+    
+    data_frames = [
+        pd.read_excel(links[year], sheet_name='Direct Emitters', skiprows=3).assign(year=year)
+        for year in years if year in links
+    ]
+
+    concat_data = pd.concat(data_frames, ignore_index = True)
+    
+    return concat_data
