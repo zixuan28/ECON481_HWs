051e83b02100dc3c29bbf50befa1969acf985e2e
zixuan28 <zlin28@uw.edu>
2024-04-11 17:15:02 -0700

    new changes

--- 481HW3.py
+++ 481HW3.py
@@ -10,6 +10,9 @@ def github() -> str:
 
 #Ex 1:
 def import_yearly_data(years: list) -> pd.DataFrame:
+    '''
+    This function takes a list of years and returns a dataframe with the data from the years in the list.
+    '''
     links = {
         2022: "https://lukashager.netlify.app/econ-481/data/ghgp_data_2022.xlsx",
         2021: "https://lukashager.netlify.app/econ-481/data/ghgp_data_2021.xlsx",
@@ -18,10 +21,10 @@ def import_yearly_data(years: list) -> pd.DataFrame:
     }
     
     data_frames = [
-        pd.read_excel(links[year], sheet_name='Direct Emitters', skiprows=3).assign(year=year)
-        for year in years if year in links
+        pd.read_excel(links[year], sheet_name='Direct Emitters', skiprows=3).assign(year=year) #reading data with year in the link
+        for year in years if year in links #making sure the year is in the links
     ]
 
-    concat_data = pd.concat(data_frames, ignore_index = True)
+    concat_data = pd.concat(data_frames, ignore_index = True) #concatenating the dataframes without the original indices from the sets
     
     return concat_data
